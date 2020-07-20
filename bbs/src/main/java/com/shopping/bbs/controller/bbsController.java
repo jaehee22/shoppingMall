5e6aa59577e605f952cf6f53aac3a88d101c15f3
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-20 19:51:54 +0900

    Create bbsController.java

---
+++ bbs/src/main/java/com/shopping/bbs/controller/bbsController.java
@@ -0,0 +1,38 @@
+package com.shopping.bbs.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+import com.shopping.bbs.dto.bbsDTO;
+import com.shopping.bbs.form.bbsForm;
+import com.shopping.bbs.service.bbsService;
+ 
+@Controller
+@RequestMapping(value = "/bbs")
+public class bbsController {
+ 
+    @Autowired
+    private bbsService bbsService;
+ 
+    @RequestMapping( value = "/bbs")
+    public String BList(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        
+        return "bbs/bbs";
+    }
+ 
+    @RequestMapping(value = "/BbsList")
+    @ResponseBody
+    public List<bbsDTO> BbsList(HttpServletRequest request, HttpServletResponse response, bbsForm bbsForm) throws Exception {
+ 
+        List<bbsDTO> bbsList = bbsService.BbsList(bbsForm);
+ 
+        return bbsList;
+    }
+}
