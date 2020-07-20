cc3f1b8acb8c1822050737f2304d4cd98f6ff415
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-20 19:52:03 +0900

    Create bbsDTO.java

---
+++ bbs/src/main/java/com/shopping/bbs/dto/bbsDTO.java
@@ -0,0 +1,54 @@
+package com.shopping.bbs.dto;
+
+public class bbsDTO {
+	int bbsID;
+	int category;
+	int subCategory;
+	String titls;
+	int price;
+	String content;
+	int sell;
+	public int getBbsID() {
+		return bbsID;
+	}
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public int getSubCategory() {
+		return subCategory;
+	}
+	public void setSubCategory(int subCategory) {
+		this.subCategory = subCategory;
+	}
+	public String getTitls() {
+		return titls;
+	}
+	public void setTitls(String titls) {
+		this.titls = titls;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getSell() {
+		return sell;
+	}
+	public void setSell(int sell) {
+		this.sell = sell;
+	}
+	
+}
