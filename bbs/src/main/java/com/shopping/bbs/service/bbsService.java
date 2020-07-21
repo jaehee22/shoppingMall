ba543677d65cc5d178dfa289802a952de4ff597e
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-21 15:18:49 +0900

    Update bbsService.java

--- bbs/src/main/java/com/shopping/bbs/service/bbsService.java
+++ bbs/src/main/java/com/shopping/bbs/service/bbsService.java
@@ -15,8 +15,65 @@ public class bbsService {
     @Autowired
     private bbsDAO bbsDAO;
  
+    //게시판 목록
     public List<bbsDTO> BbsList(bbsForm bbsForm) throws Exception {
  
         return bbsDAO.BbsList(bbsForm);
+    }
+    
+    //게시판 뷰
+    public bbsDTO BbsView(bbsForm bbsForm) throws Exception {
+    	
+    	bbsDTO bbsDTO = new bbsDTO();
+    	
+    	int cnt = bbsDAO.BbsView(bbsForm);
+    	
+    	if(cnt>0) {
+    		bbsDTO.setResult("SUCCESS");
+    	}
+    	
+    	return bbsDTO;
+    }
+    
+    //게시글 등록
+    public bbsDTO BbsWrite(bbsForm bbsForm) throws Exception{
+    	
+    	bbsDTO bbsDTO = new bbsDTO();
+    	
+    	int cnt = bbsDAO.BbsWrite(bbsForm);
+    	
+    	if(cnt>0) {
+    		bbsDTO.setResult("SUCCESS");
+    	}
+    	
+    	return bbsDTO;
+    }
+    
+    //게시글 삭제
+    public bbsDTO BbsDelete(bbsForm bbsForm) throws Exception{
+    	
+    	bbsDTO bbsDTO = new bbsDTO();
+    	
+    	int cnt = bbsDAO.BbsDelete(bbsForm);
+    	
+    	if(cnt>0) {
+    		bbsDTO.setResult("SUCCESS");
+    	}
+    	
+    	return bbsDTO;
+    }
+    
+    //게시글 수정
+    public bbsDTO BbsUpdate(bbsForm bbsForm) throws Exception{
+    	
+    	bbsDTO bbsDTO = new bbsDTO();
+    	
+    	int cnt = bbsDAO.BbsUpdate(bbsForm);
+    	
+    	if(cnt>0) {
+    		bbsDTO.setResult("SUCCESS");
+    	}
+    	
+    	return bbsDTO;
     }
 }
