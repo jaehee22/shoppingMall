298332128537537ee0eb60d7ccfb2f5defe55642
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-23 18:09:04 +0900

    Update bbs.js

--- bbs/src/main/webapp/resources/js/bbs.js
+++ bbs/src/main/webapp/resources/js/bbs.js
@@ -1,21 +1,49 @@
  $(document).ready(function(){        
-        BbsList();
+        BbsbbsList();
+        Category();
     });
- 
-    function BbsList(){
-        
-        $.ajax({            
-            type:"GET",
-            url:"/bbs/BbsList",
-            dataType:"JSON",
-            success : function(obj) {
-                BbsListCallback(obj);                
-            },           
-            error : function(xhr, status, error) {}
-         });
+ 	
+    function BbsbbsList(category,subCategory){
+		var category = $("#category").val();
+		var subCategory = $("#subCategory").val();
+		
+		if(category!=0 && subCategory!=0){
+			$.ajax({    
+	  
+	            url        : "/bbs/BbsbbsList",
+	            data    : $("#category").serialize()+"&"+$("#subCategory").serialize(),
+	            dataType: "JSON",
+	            cache   : false,
+	            async   : true,
+	            type    : "POST",    
+	            success : function(obj) {
+	                BbsbbsListCallback(obj);                
+	            },           
+	            error     : function(xhr, status, error) {}
+	            
+	         });
+		}
     }
     
-    function BbsListCallback(obj){
+    function Category(category,subCategory){
+    	
+    	var category = $("#category").val();
+		var subCategory = $("#subCategory").val();
+		str = "";
+		
+    	var sub = new Array('','귀걸이','목걸이','반지','팔찌','발찌');	
+    	sub[1] = new Array('귀걸이','피어싱','귀찌','이어커프');
+    	sub[2] = new Array('목걸이','초커','패션 목걸이');
+    	sub[3] = new Array('반지','레이어드 링','은반지','금반지');
+    	sub[4] = new Array('팔찌','가죽 팔찌','원석 팔찌','은 팔찌');
+    	sub[5] = new Array('발찌');
+    	
+    	str += sub[category][0]+">"+sub[category][subCategory-1];
+    	
+    	$("#tcategory").html(str);
+    }
+    
+    function BbsbbsListCallback(obj){
         
         var list = obj;
         var listLen = obj.length;
