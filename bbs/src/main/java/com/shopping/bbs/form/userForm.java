22b7ad0a8834911be70148793b60a5f9c5061f32
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-24 13:00:22 +0900

    Create userForm.java

---
+++ bbs/src/main/java/com/shopping/bbs/form/userForm.java
@@ -0,0 +1,40 @@
+package com.shopping.bbs.form;
+
+public class userForm {
+	String userID;
+	int userPassword;
+	String userName;
+	String userGender;
+	int isManager;
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public int getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(int userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public int getIsManager() {
+		return isManager;
+	}
+	public void setIsManager(int isManager) {
+		this.isManager = isManager;
+	}
+	
+}
