44a0464198e96a8a0d68de2d1e510b7f27b046b1
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-27 15:21:39 +0900

    Update userController.java

--- bbs/src/main/java/com/shopping/bbs/controller/userController.java
+++ bbs/src/main/java/com/shopping/bbs/controller/userController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.shopping.bbs.dto.bbsDTO;
 import com.shopping.bbs.dto.userDTO;
 import com.shopping.bbs.form.userForm;
 import com.shopping.bbs.service.userService;
