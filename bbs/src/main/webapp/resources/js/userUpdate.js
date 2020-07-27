a4a6a45afa5f2841f6d8821feeab7e2391d521a2
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-07-27 13:08:46 +0900

    Create userUpdate.js

---
+++ bbs/src/main/webapp/resources/js/userUpdate.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){        
+	UserView();
+});
+    
+	//게시판 목록 이동
+	function goBbsList(){                
+	    location.href = "/bbs/home";
+	}
+
+	//회원정보
+	function UserView(userID){
+		var userID = $("#userID").val();
+		if(userID!=null){
+			$.ajax({    
+	  
+	            url        : "/user/UserView",
+	            data    : $("#userForm").serialize(),
+	            dataType: "JSON",
+	            cache   : false,
+	            async   : true,
+	            type    : "POST",    
+	            success : function(obj) {
+	                UserViewCallback(obj);                
+	            },           
+	            error     : function(xhr, status, error) {
+	            	alert("등록된 유저가 존재하지 않습니다.");
+	            	//location.href = "/bbs/home"
+	            } 
+	            
+	         });
+	    } else {
+	        alert("오류가 발생했습니다.\n관리자에게 문의하세요.");
+		}
+	}
+   function UserViewCallback(obj){
+        var str = "";
+        if(obj != null){                                
+            var userID = obj.userID;
+            var userPassword = obj.userPassword;
+            var userName = obj.userName;
+            var userGender = obj.userGender;
+            
+            $("#userID").val(userID);
+            $("#userPassword").val(userPassword);
+            $("#userName").val(userName);
+            $("#userGender").val(userGender);
+            
+            if( userGender == "여자" ){
+            	$("input:radio[name='userGender']:radio[value='여자']").attr("checked", true);
+            	} else {
+            	$("input:radio[name='userGender']:radio[value='남자']").attr("checked", true);
+            	}
+            
+        } else {
+            
+            return;
+        }        
+        
+    }
+	   
+//회원 수정
+function UserUpdate(){
+	
+	var userID = $("#userID").val();
+    var userPassword = document.forms["userForm2"].elements["userPassword"].value;
+    var userName = document.forms["userForm2"].elements["userName"].value;
+    var userGender = document.forms["userForm2"].elements["userGender"].value;
+    
+    alert(userPassword+"<"+userName+"<"+userGender);
+    if (userPassword == 0){            
+        alert("비밀번호를 입력해 주세요");
+        $("#userPassword").focus();
+        return;
+    }
+    
+    if (userName == ""||userName==null){            
+        alert("이름을 입력해주세요.");
+        $("#userName").focus();
+        return;
+    }
+    
+    if (userGender == ""||userName==null){            
+        alert("성별을 체크해주세요.");
+        $("#userGender").focus();
+        return;
+    }
+    
+    var yn = confirm("회원정보를 수정하시겠습니까?");        
+    if(yn){
+            
+        $.ajax({    
+            
+            url        : "/user/UserUpdate",
+            data    : "userPassword="+userPassword+"&userName="+userName+"&userGender="+userGender+"&userID="+userID,
+            dataType: "JSON",
+            cache   : false,
+            async   : true,
+            type    : "POST",    
+            success : function(obj) {
+                UserUpdateCallback(obj);                
+            },           
+            error     : function(xhr, status, error) {}
+            
+        });
+    }
+}
+
+//게시글 수정 함수
+function UserUpdateCallback(obj){	
+	
+    if(obj != null){        
+        
+    	var result = obj.result;
+                
+        if(result == "SUCCESS"){                
+            alert("회원 수정을 성공하였습니다.");                
+            //goBbsList();                 
+        } else {                
+            alert("회원 수정을 실패하였습니다.");    
+            return;
+        }
+    }
+}
