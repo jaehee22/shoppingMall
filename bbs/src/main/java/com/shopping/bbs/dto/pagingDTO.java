d8aa63d750ab5cc0c34f667f2e8b2de1f2407e3a
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-08-03 21:26:18 +0900

    Create pagingDTO.java

---
+++ bbs/src/main/java/com/shopping/bbs/dto/pagingDTO.java
@@ -0,0 +1,43 @@
+package com.shopping.bbs.dto;
+
+public class pagingDTO {
+	private int pageNum;
+	private int startPageNum;
+	private int endPageNum;
+	private boolean prev;
+	private boolean next;
+	public int getPageNum() {
+		return pageNum;
+	}
+	public void setPageNum(int pageNum) {
+		this.pageNum = pageNum;
+	}
+	public int getStartPageNum() {
+		return startPageNum;
+	}
+	public void setStartPageNum(int startPageNum) {
+		this.startPageNum = startPageNum;
+	}
+	public int getEndPageNum() {
+		return endPageNum;
+	}
+	public void setEndPageNum(int endPageNum) {
+		this.endPageNum = endPageNum;
+	}
+	public boolean isPrev() {
+		return prev;
+	}
+	public void setPrev(boolean prev) {
+		this.prev = prev;
+	}
+	public boolean isNext() {
+		return next;
+	}
+	public void setNext(boolean next) {
+		this.next = next;
+	}
+
+	
+	
+	
+}
