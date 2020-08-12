8791ad66f71619000361e0724bc277cc68745dee
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-08-12 16:04:15 +0900

    Update home.js

--- bbs/src/main/webapp/resources/js/home.js
+++ bbs/src/main/webapp/resources/js/home.js
@@ -19,7 +19,8 @@
         
         var list = obj;
         var listLen = obj.length;
-        
+        var imgPath = "http://localhost:8080/resources/bbsImg/";
+
         console.log(list);
         console.log(listLen);
         
@@ -36,12 +37,14 @@
                 var price = list[a].price;
                 var sell = list[a].sell;
                 
+                var filePath = imgPath+bbsID+"/"+bbsID+".jpg";
+                
                 str += "<div class=\"col-lg-4 col-md-6 mb-4\">";
                 str += "<div class=\"card h-100\">";
-                str += "<a href=\"/bbs/bbsView?bbsID="+bbsID+"\"><img class=\"card-img-top\" src=\"http://placehold.it/700x400\" alt=\"\"></a>";
+                str += "<a href=\"/bbs/bbsView?category="+category+"&subCategory="+subCategory+"&bbsID="+bbsID+"\"><img class=\"card-img-top\" src=\""+filePath+"\" onerror=\"this.src='http://placehold.it/700x400'\" width=\"300\" height=\"171\" alt=\"\"/></a>";
                 str += "<div class=\"card-body\">";
                 str += "<h4 class=\"card-title\">";
-                str += "<a href=\"/bbs/bbsView?bbsID="+bbsID+"\">"+title+"</a>";
+                str += "<a href=\"/bbs/bbsView?category="+category+"&subCategory="+subCategory+"&bbsID="+bbsID+"\">"+title+"</a>";
                 str += "</h4>";
                 str += "<h5>"+price+"원</h5>";
                 str += "</div>";
