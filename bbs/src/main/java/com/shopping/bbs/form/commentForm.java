877cdff3fcfa463874ede035dc5c2d957668eace
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-08-14 16:38:07 +0900

    Create commentForm.java

---
+++ bbs/src/main/java/com/shopping/bbs/form/commentForm.java
@@ -0,0 +1,77 @@
+package com.shopping.bbs.form;
+
+public class commentForm {
+	private int commentID;
+	private int bbsID;
+	private int comCategory;
+	private String userID;
+	private String content;
+	
+	String result;
+	int num;
+	int displayPost;
+	int postNum;
+	int pageNum;
+	
+	public int getBbsID() {
+		return bbsID;
+	}
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	public int getComCategory() {
+		return comCategory;
+	}
+	public void setComCategory(int comCategory) {
+		this.comCategory = comCategory;
+	}
+	public int getCommentID() {
+		return commentID;
+	}
+	public void setCommentID(int commentID) {
+		this.commentID = commentID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getDisplayPost() {
+		return displayPost;
+	}
+	public void setDisplayPost(int displayPost) {
+		this.displayPost = displayPost;
+	}
+	public int getPostNum() {
+		return postNum;
+	}
+	public void setPostNum(int postNum) {
+		this.postNum = postNum;
+	}
+	public int getPageNum() {
+		return pageNum;
+	}
+	public void setPageNum(int pageNum) {
+		this.pageNum = pageNum;
+	}
+	
+}
