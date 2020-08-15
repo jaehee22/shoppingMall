95bc35995c3dc925614098b31e728fccff43ff46
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-08-15 17:59:22 +0900

    Update bbsView.js

--- bbs/src/main/webapp/resources/js/bbsView.js
+++ bbs/src/main/webapp/resources/js/bbsView.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){        
     BbsView();
+    CommentList();
 });
     
 	//게시판 목록 이동
@@ -87,6 +88,82 @@ $(document).ready(function(){
 	        $("#tbody").html(str);
 	    }
 	   
+		//댓글 목록
+		function CommentList(bbsID, comCategory, commentNum){
+			
+			var commentNum = $("#commentNum").val();
+
+			if(bbsID!=0 && comCategory!=0){
+				$.ajax({    
+		  
+		            url        : "/comment/CommentList",
+		            data    : $("#bbsID").serialize()+"&"+$("#comCategory").serialize()+"&num="+commentNum,
+		            dataType: "JSON",
+		            cache   : false,
+		            async   : true,
+		            type    : "POST",    
+		            success : function(obj) {
+		                CommentListCallback(obj);                
+		            },           
+		            error     : function(xhr, status, error) {}
+		            
+		         });
+		    } else {
+		        alert("오류가 발생했습니다.\n관리자에게 문의하세요.");
+			}
+		}
+	   function CommentListCallback(obj){
+		   	
+		   var num = $("#commentNum").val();
+		   	
+	        var list = obj;
+	        var listLen = obj.length;
+	        console.log(list);
+	        console.log(listLen);
+
+	        var str = "";
+	        
+	        if(listLen >  0){
+	            
+	            for(var a=0; a<listLen; a++){  
+	            	
+		        	var commentID = list[a].commentID;
+		        	var bbsID = list[a].bbsID;
+		            var userID = list[a].userID;
+		            var content = list[a].content;
+		            
+			        var filePath = "http://localhost:8080/resources/bbsImg/"+bbsID+"/comment/"+commentID+".jpg";
+		           
+			        str += "<div class=\"container\">";
+		            str += "<div class=\"row\">";
+		            str += "<table class=\"table table-striped\" style=\"text-align:center; border: 1px solid #dddddd\">";
+		            str += "<tr>";
+		            str += "<td align=\"left\">"+userID+"></td>"
+		            str += "<td colspan=2></td>";
+		            str += "<td align=\"right\">";
+		            str += "<c:if test=\"${userForm.userID}=="+userID+"\">";
+		            str += "<button type=\"button\" onclick=\"javascript:CommentUpdate();\">수정하기</button>";
+		            str += "<button type=\"button\" onclick=\"javascript:CommentDelete();\">삭제하기</button>";
+		            str += "</c:if>";
+		            str += "</td>";
+		            str += "</tr>";
+		            str += "<tr>";
+		            str += "<td colspan=5 align=\"left\">";
+		            str += "<img src=\""+filePath+"\" onerror=\"this.style.display='none'\" width=\"200\" height=\"200\" alt=\"\" /><br><br>"+content+"<br><br></td>";
+		            str += "</tr>";
+		            str += "</table>";
+		            str += "</div>";
+		            str += "</div>";
+		          
+	            }
+	        } else {
+	            return;
+	        }        
+	        
+	        $("#tcomment").html(str);
+	    }
+   
+	   
 	   //게시글 삭제
 	   function BbsDelete(){
 	
@@ -129,13 +206,14 @@ $(document).ready(function(){
 	   }
 	   
    //댓글 작성
-   function CommentWrite(bbsID, bbsCategory, userID){
+   function CommentWrite(bbsID,	comCategory, userID){
 	   
 	   var bbsID = $("#bbsID").val();
-       var comCategory = $("#bbsCategory").val();
+       var comCategory = $("#comCategory").val();
        var userID = $("userID").val()
 	   
 	   var yn = confirm("댓글을 등록하시겠습니까?");        
+       
        if(yn){
            
        	var form = $('#commentForm')[0];
@@ -165,14 +243,18 @@ $(document).ready(function(){
 
    //댓글 작성 함수
    function CommentWriteCallback(obj){
-
+	   
+	   var bbsID = $("#bbsID").val();
+       var comCategory = $("#comCategory").val();
+	   var commentNum = $("#commentNum").val();
+       
        if(obj != null){        
            
            var result = obj.result;
            
            if(result == "SUCCESS"){                
                alert("댓글 등록을 성공하였습니다.");                
-               goBbsList();                 
+               location.href = "/bbs/bbsView?bbsID="+encodeURI(bbsID)+"&comCategory="+comCategory+"&commentNum="+encodeURI(commentNum);
            } else {                
                alert("댓글 등록을 실패하였습니다.");    
                return;
