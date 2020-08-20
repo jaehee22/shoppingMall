b925e5683535a1b8235244d548d35d67d600fd1f
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-08-20 16:48:27 +0900

    Update cartBbs.js

--- bbs/src/main/webapp/resources/js/cartBbs.js
+++ bbs/src/main/webapp/resources/js/cartBbs.js
@@ -41,6 +41,7 @@
             
             for(var a=0; a<listLen; a++){            	            	
             	
+            	var cartID = list[a].cartID;
             	var bbsID = list[a].bbsID;
                 var title = list[a].title;
                 var price = list[a].price;
@@ -49,13 +50,14 @@
                 var filePath = imgPath+bbsID+"/"+bbsID+".jpg";
                 
                 str += "<tr>";
+                str += "<td><br><input type=\"checkbox\" name=\"cartCheck\"/></td>"
                 str += "<td><a href=\"/bbs/bbsView?bbsID="+bbsID+"&comCategory=1&commentNum=1\"><img src=\""+filePath+"\" onerror=\"this.src='http://placehold.it/700x400'\" width=\"80\" height=\"80\" alt=\"\"/></a></td>";
                 str += "<td><a href=\"/bbs/bbsView?bbsID="+bbsID+"&comCategory=1&commentNum=1\"><br>"+title+"</a></td>";
                 str += "<td><br>"+price+"원</td>";
                 str += "<td><br>"+amount+"</td>";
                 str += "<td><br>"+(price*amount)+"원</td>"
-                str += "<td><br><button type=\"button\" onclick=\"/bbs/bbsOrder\">주문</button>";
-                str += "<button type=\"button\" onclick=\"javascript:cartDelete();\">삭제</button></td>";
+                str += "<td><br><button type=\"button\" onclick=\"/bbs/bbsOrder\">주문</button>&emsp;";
+                str += "<button type=\"button\" onclick=\"javascript:CartDelete("+cartID+");\">삭제</button></td>";
                 str += "</tr>";
             } 
         } else {
@@ -126,4 +128,45 @@
         }
  		
  	}
-    
+ 	
+ 	//장바구니 상품 삭제
+ 	 function CartDelete(cartID){
+
+ 		var yn = confirm("장바구니에 담긴 상품을 삭제하시겠습니까?");        
+        
+        if(yn){
+ 			$.ajax({    
+ 	  
+ 	            url        : "/cart/CartDelete",
+ 	            data    : "cartID="+cartID,
+ 	            dataType: "JSON",
+ 	            cache   : false,
+ 	            async   : true,
+ 	            type    : "POST",    
+ 	            success : function(obj) {
+ 		                CartDeleteCallback(obj);                
+ 	            },           
+ 	            error     : function(xhr, status, error) {}
+ 	            
+ 	         });
+ 		}
+     }
+ 	 
+ 	//장바구니 삭제 함수
+	function CartDeleteCallback(obj){	
+
+		var cartNum = $("#cartNum").val();
+		
+	    if(obj != null){        
+	        
+	    	var result = obj.result;
+	                
+	        if(result == "SUCCESS"){                
+	            alert("장바구니의 상품을 삭제하였습니다.");
+	            location.href = "/cart/cartBbs?cartNum="+cartNum;
+	        } else {                
+	            alert("상품 삭제를 실패하였습니다.");    
+	            return;
+	        }
+	    }
+	}
