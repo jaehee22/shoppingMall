6aff4ae180ade93f464beba530b69f3fe822c8d9
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-08-20 16:29:14 +0900

    Create cartService.java

---
+++ bbs/src/main/java/com/shopping/bbs/service/cartService.java
@@ -0,0 +1,83 @@
+package com.shopping.bbs.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+ 
+import com.shopping.bbs.dao.cartDAO;
+import com.shopping.bbs.dto.cartDTO;
+import com.shopping.bbs.form.cartForm;
+ 
+@Service
+public class cartService {
+ 
+    @Autowired
+    private cartDAO cartDAO;
+ 
+    //카트 목록
+    public List<cartDTO> CartList(cartForm cartForm) throws Exception {
+    	    	
+        return cartDAO.CartList(cartForm);
+    }
+
+    //카트 총 갯수
+    public int CartTotal(cartForm cartForm) throws Exception{
+    	return cartDAO.CartTotal(cartForm);
+    }
+    
+    //카트 뷰
+    public cartDTO CartView(cartForm cartForm) throws Exception {
+    	
+    	cartDTO cartDTO = new cartDTO();
+    	
+    	cartDTO = cartDAO.CartView(cartForm);
+    	
+    	return cartDTO;
+    }
+    
+    //카트 등록
+    public cartDTO CartWrite(cartForm cartForm) throws Exception{
+    	
+    	cartDTO cartDTO = new cartDTO();
+    
+        cartForm.setCartID(cartDAO.GetNext());
+    	
+    	int cnt = cartDAO.CartWrite(cartForm);
+    	
+    	if(cnt>0) {
+    		cartDTO.setResult("SUCCESS");
+    	}
+    	
+    	return cartDTO;
+    }
+    
+    //카트 삭제
+    public cartDTO CartDelete(cartForm cartForm) throws Exception{
+    	
+    	cartDTO cartDTO = new cartDTO();
+    	
+    	int cnt = cartDAO.CartDelete(cartForm);
+    	
+    	if(cnt>0) {
+    		cartDTO.setResult("SUCCESS");
+    	}
+    	
+    	return cartDTO;
+    }
+    
+    //카트 수정
+    public cartDTO CartUpdate(cartForm cartForm) throws Exception{
+    	
+    	cartDTO cartDTO = new cartDTO();
+    	
+    	int cnt = cartDAO.CartUpdate(cartForm);
+    	
+    	if(cnt>0) {
+    		cartDTO.setResult("SUCCESS");
+    	}
+    	
+    	return cartDTO;
+    }
+    
+}
