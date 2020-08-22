8cf40dbf650b7e1cdb2449aa03a724a332ba06b2
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-08-22 18:42:01 +0900

    Create orderDTO.java

---
+++ bbs/src/main/java/com/shopping/bbs/dto/orderDTO.java
@@ -0,0 +1,116 @@
+package com.shopping.bbs.dto;
+
+public class orderDTO {
+	private int orderID;
+	private String userID;
+	private String orderName;
+	private String addr1;
+	private String addr2;
+	private String addr3;
+	private String phoneNumber;
+	private String orderMemo;
+	private String delivery;
+	
+	private int bbsID;
+	private int amount;
+	
+	private String result;
+
+	public int getOrderID() {
+		return orderID;
+	}
+
+	public void setOrderID(int orderID) {
+		this.orderID = orderID;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getOrderName() {
+		return orderName;
+	}
+
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+
+	public String getAddr1() {
+		return addr1;
+	}
+
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+
+	public String getAddr2() {
+		return addr2;
+	}
+
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+
+	public String getAddr3() {
+		return addr3;
+	}
+
+	public void setAddr3(String addr3) {
+		this.addr3 = addr3;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getOrderMemo() {
+		return orderMemo;
+	}
+
+	public void setOrderMemo(String orderMemo) {
+		this.orderMemo = orderMemo;
+	}
+
+	public String getDelivery() {
+		return delivery;
+	}
+
+	public void setDelivery(String delivery) {
+		this.delivery = delivery;
+	}
+
+	public int getBbsID() {
+		return bbsID;
+	}
+
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	
+}
