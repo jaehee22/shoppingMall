ac43fb08d8d6ddf6dba7c42754bcaed25bbbfa07
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-08-24 20:48:53 +0900

    Update userUpdate.js

--- bbs/src/main/webapp/resources/js/userUpdate.js
+++ bbs/src/main/webapp/resources/js/userUpdate.js
@@ -2,61 +2,60 @@ $(document).ready(function(){
 	UserView();
 });
     
-	//게시판 목록 이동
-	function goBbsList(){                
-	    location.href = "/bbs/home";
-	}
+//게시판 목록 이동
+function goBbsList(){                
+    location.href = "/bbs/home";
+}
 
-	//회원정보
-	function UserView(userID){
-		var userID = $("#userID").val();
-		if(userID!=null){
-			$.ajax({    
-	  
-	            url        : "/user/UserView",
-	            data    : $("#userForm").serialize(),
-	            dataType: "JSON",
-	            cache   : false,
-	            async   : true,
-	            type    : "POST",    
-	            success : function(obj) {
-	                UserViewCallback(obj);                
-	            },           
-	            error     : function(xhr, status, error) {
-	            	alert("등록된 유저가 존재하지 않습니다.");
-	            	location.href = "/bbs/home"
-	            } 
-	            
-	         });
-	    } else {
-	        alert("오류가 발생했습니다.\n관리자에게 문의하세요.");
-		}
+//회원정보
+function UserView(){
+	var userID = $("#userID").val();
+	if(userID!=null){
+		$.ajax({    
+  
+            url        : "/user/UserView",
+            data    : $("#userForm").serialize(),
+            dataType: "JSON",
+            cache   : false,
+            async   : true,
+            type    : "POST",    
+            success : function(obj) {
+                UserViewCallback(obj);                
+            },           
+            error     : function(xhr, status, error) {
+            	alert("등록된 유저가 존재하지 않습니다.");
+            	location.href = "/bbs/home"
+            } 
+            
+         });
+    } else {
+        alert("오류가 발생했습니다.\n관리자에게 문의하세요.");
 	}
-   function UserViewCallback(obj){
-        var str = "";
-        if(obj != null){                                
-            var userID = obj.userID;
-            var userPassword = obj.userPassword;
-            var userName = obj.userName;
-            var userGender = obj.userGender;
-            
-            $("#userID").val(userID);
-            $("#userPassword").val(userPassword);
-            $("#userName").val(userName);
-            $("#userGender").val(userGender);
-            
-            if( userGender == "여자" ){
-            	$("input:radio[name='userGender']:radio[value='여자']").attr("checked", true);
-            	} else {
-            	$("input:radio[name='userGender']:radio[value='남자']").attr("checked", true);
-            	}
-            
-        } else {
-            
-            return;
-        }        
+}
+function UserViewCallback(obj){
+	var str = "";
+	if(obj != null){                                
+		var userID = obj.userID;
+		var userPassword = obj.userPassword;
+		var userName = obj.userName;
+		var userGender = obj.userGender;
         
-    }
+        $("#userID").val(userID);
+        $("#userPassword").val(userPassword);
+        $("#userName").val(userName);
+        $("#userGender").val(userGender);
+        
+        if( userGender == "여자" ){
+        	$("input:radio[name='userGender']:radio[value='여자']").attr("checked", true);
+        	} else {
+        	$("input:radio[name='userGender']:radio[value='남자']").attr("checked", true);
+        	}
+        
+    } else { 	
+    	return;
+    }        
+    
+}
 	   
 //회원 수정
 function UserUpdate(){
@@ -127,13 +126,12 @@ function UserUpdateCallback(obj){
 function UserDelete(){
 	
 	var userID = $("#userID").val();
-	
     var yn = confirm("회원탈퇴를 하시겠습니까?");        
     
 	if(yn){
 		$.ajax({    
   
-            url        : "/user/UserDelete",
+			url        : "/user/UserDelete",
             data    : $("#userForm").serialize(),
             dataType: "JSON",
             cache   : false,
@@ -147,9 +145,6 @@ function UserDelete(){
             	alert("등록된 유저가 존재하지 않습니다.");
             	location.href = "/bbs/home"
             } 
-            
-			});
-		}
-	
-	
+		});
+	}
 }
